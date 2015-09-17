39390a17e36a9eb9b85678d516c585bf39f003c7
brendan <brendan.heberton@gmail.com>
2015-09-17 14:54:51 -0600

    chart and stats for narrative page

--- app/routes/narrative.js
+++ app/routes/narrative.js
@@ -1,19 +1,32 @@
 import Ember from 'ember';
 
 var json = {
-  dataset: null,
   '14faf3d4bfbe4ca4a713bf203a985151_0': {
-    name: 'All Service Requests!'
+    'name': 'All Service Requests - Last 30 Days',
+    'description': 'This data layer shows 311 Service Requests in the last 30 days for all service request types.',
+    'section1': {
+      'section_title': 'Your city at work',
+      'feature1': {
+        'feature_type': 'stat',
+        'feature_number': 451,
+        'feature_title': 'Potholes Filled Last 30 Days',
+        'feature_sub_title': '<i class="glyphicon glyphicon-arrow-up green"></i> up 55% from last month'
+      },
+      'feature2': {
+        'type': 'chart',
+        'feature_chart_id': 'potholeChart',
+        'feature_title': 'Number of Potholes Filled in D.C. by Month',
+        'feature_data': {}
+      }
+    }
   }
 }
-
 
 export default Ember.Route.extend({
 
   model: function (params) {
-
-    json.dataset = this.store.findRecord('dataset', params.id);
-    return json;
+    //return this.store.findRecord('dataset', params.id);
+    return json[params.id];
   }
 
 });
