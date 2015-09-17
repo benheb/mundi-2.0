c88c4ff4b16fbe44c308043ea38c8c3a4c35928d
brendan <brendan.heberton@gmail.com>
2015-09-17 13:33:19 -0600

    working on page

--- app/routes/narrative.js
+++ app/routes/narrative.js
@@ -1,9 +1,19 @@
 import Ember from 'ember';
 
+var json = {
+  dataset: null,
+  '14faf3d4bfbe4ca4a713bf203a985151_0': {
+    name: 'All Service Requests!'
+  }
+}
+
+
 export default Ember.Route.extend({
 
   model: function (params) {
-    return this.store.findRecord('dataset', params.id);
+
+    json.dataset = this.store.findRecord('dataset', params.id);
+    return json;
   }
 
 });
