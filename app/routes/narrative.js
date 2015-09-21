f0c5faa638a46cc3c4e175239615c06e6ea6c670
brendan <brendan.heberton@gmail.com>
2015-09-21 09:57:04 -0600

    fix a minor bug

--- app/routes/narrative.js
+++ app/routes/narrative.js
@@ -16,8 +16,10 @@ export default Ember.Route.extend({
 
     if ( model.get('id') === '14faf3d4bfbe4ca4a713bf203a985151_0' ) {
       json.service_reqs = true;
+      json.vision_zero = false;
     } else if ( model.get('id') === '3f28bc3ad77f49079efee0ac05d8464c_0' ) {
       json.vision_zero = true;
+      json.service_reqs = false;
     }
 
     controller.set('page', json);
