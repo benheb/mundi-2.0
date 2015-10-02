cb93a9fb1d0cc90a80862725a26813e6e74f6e38
mjuniper <mjuniper@gmail.com>
2015-10-02 09:49:06 -0600

    fetch \'em

--- app/instance-initializers/i18n.js
+++ app/instance-initializers/i18n.js
@@ -1,24 +1,41 @@
+import Ember from 'ember';
+import { request } from 'ic-ajax';
+const { keys } = Ember;
+
+//const PATH = '/my/api/translations.json';
+
 export default {
+
   name: 'i18n',
-  initialize: function(application) {
-    var svc = application.container.lookup('service:i18n');
-    var locale = this.calculateLocale(svc);
-    svc.set('locale', locale);
+
+  initialize(application) {
+    let svc = application.container.lookup('service:i18n');
+    let locale = this._calculateLocale();
+    // if we got null, we want to use the default locale
+    // which we should already have in the browser...
+    if (locale && locale !== svc.get('locale'))
+    this._fetchTranslation(locale).then(function (resp) {
+      svc.addTranslations(locale, resp);
+      svc.set('locale', locale);
+    }.bind(this));
   },
 
-  calculateLocale: function (svc) {
+  _fetchTranslation(locale) {
+    const url = '/locales/' + locale + '/translations.json';
+    return request(url);
+  },
+
+  _calculateLocale() {
     var nav = window.navigator,
       browserLanguagePropertyKeys = ['language', 'browserLanguage', 'systemLanguage', 'userLanguage'],
       i,
       language;
 
     // support for HTML 5.1 "navigator.languages"
-    if (Array.isArray(nav.languages)) {
-      for (i = 0; i < nav.languages.length; i++) {
-        language = nav.languages[i];
-        if (language && language.length) {
-          return language;
-        }
+    if (Array.isArray(nav.languages) && nav.languages.length > 0) {
+      language = nav.languages[0];
+      if (language && language.length) {
+        return language;
       }
     }
 
@@ -30,6 +47,6 @@ export default {
       }
     }
 
-    return svc.get('locale');
+    return null;
   }
 };
