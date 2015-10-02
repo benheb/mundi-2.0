8285a1af2c17161c7289f245716c8f08d49e1706
mjuniper <mjuniper@gmail.com>
2015-10-02 07:01:32 -0600

    scaffolding

---
+++ tests/unit/routes/dataset-table/:id-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:dataset-table/:id', 'Unit | Route | dataset table/:id', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
