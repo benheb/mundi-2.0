095e451747ac1cadb30f016d35bd889917ecba22
brendan <brendan.heberton@gmail.com>
2015-10-06 09:11:40 -0600

    working on tests

--- tests/integration/components/od-range-slider-test.js
+++ tests/integration/components/od-range-slider-test.js
@@ -2,21 +2,27 @@ import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('od-range-slider', 'Integration | Component | od range slider', {
-  integration: false
+  integration: true
 });
 
 test('it renders', function(assert) {
   assert.expect(3);
 
-  var component = this.subject({ filterMin: 10, filterMax: 100, selectedAttribute: '' });
-  this.render();
-
-  Ember.run(function () {
-    component.set('filterMin', 1);  
+  let model = Ember.Object.create({
+    fields: [{'field1': 'field1', 'field2': 'field2'}]
   });
+  this.set('model', model);
   
-  assert.equal(component.get('filterMin'), 1);
-  assert.equal(this.$('#slider-min-value').text(), 1);
+  this.set('filterMin', 10);  
+  this.set('filterMax', 100);
+  this.set('selectedAttribute', '');
+
+  this.render(hbs`{{od-range-slider model=model selectedAttribute=selectedAttribute filterMin=filterMin filterMax=filterMax}}`);
+
+  assert.equal(this.$('#slider-min-value').text(), 10);
   assert.equal(this.$('#slider-max-value').text(), 100);
+
+  this.set('filterMin', 1);
+  assert.equal(this.$('#slider-min-value').text(), 1);
 
 });
